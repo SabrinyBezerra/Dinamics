96187f84ca2df8c1f6ca4be30b32e7c0f9eb9a09
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-11-01 21:54:07 -0200

    Update Feed.py

--- Dinamics/Feed.py
+++ Dinamics/Feed.py
@@ -2,36 +2,58 @@ import sqlite3
 conn = sqlite3.connect('dinamics.db')
 cursor = conn.cursor()
 
-lista = [(1, 1, 'BOM DIA GENTE'),
-         (2, 2, 'BOA TARDE GENTE'),
-         (3, 3, 'BOA NOITE GALERA')]
 
-cursor.executemany("""
-INSERT INTO TB_Feed (id, id_publicacao, publicacao)
-VALUES (?, ?, ?)
-""", lista)
+class Feed():
+    def __init__(self, id_publicacao, publicacao):
+        self.id_publicacao = id_publicacao
+        self.publicacao = publicacao
 
-cursor.execute("""
-SELECT * FROM TB_Feed;
-""")
+    def inserir(self):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+                 INSERT INTO TB_Feed (id_publicacao, publicacao)
+                 VALUES (self.id_publicacao, self.publicacao)
+               ''')
+        conn.commit()
+        conn.close()
 
-for linha in cursor.fetchall():
-    print(linha)
+    def listar(self):
+        Feed = []
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT * FROM TB_Feed;
+        """)
+        for linha in cursor.fetchall():
+            id_publicacao = linha[1]
+            publicaao= linha[2]
+            feed = feed(id_usuario, conversa)
+            feed.append(feed)
+        conn.close()
 
-id_feed = 1
-novo_publicacao = 'BOM DIA GALERA'
+        return feed
 
-cursor.execute("""
-UPDATE TB_Feed
-SET publicacao = ?
-WHERE id = ?
-""", (novo_publicacao, id_feed))
+    def deletar(self, id):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        id = input("")
+        cursor.execute("""
+        DELETE FROM TB_Fedd
+        WHERE id = ?
+        """, (id))
+        conn.commit()
+        conn.close()
 
-cursor.execute("""
-DELETE FROM TB_Feed
-WHERE id = ?
-""", (id_feed,))
+    def atualizar(self, id, id_publicacao, publicacao):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        UPDATE TB_Feed
+        SET id = ?, id_publicacao = ?, publicacao = ?
+        WHERE id = ?
+        """, (id, id_publicacao, publicacao))
+        conn.commit()
+        conn.close()
 
-conn.commit()
 
-conn.close()
