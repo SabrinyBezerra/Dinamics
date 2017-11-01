4d204ef227faa1883a545c006979bc3c619508e8
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-11-01 10:36:19 -0200

    Update Usuario.py

--- Dinamics/Usuario.py
+++ Dinamics/Usuario.py
@@ -2,37 +2,66 @@ import sqlite3
 conn = sqlite3.connect('dinamics.db')
 cursor = conn.cursor()
 
-lista = [(1, 'Sabriny', 'sabriny@gmail.com', '123456', 'estudante', 'feminino', '2001-04-26'),
-         (2, 'Pedro', 'Pedro@hotmail.com', '12345', 'estudante', 'masculino', '2001-10-09'),
-         (3, 'Larryce', 'Larryce@gmail.com', '123', 'estudante', 'feminino', '2001-10-31')]
+class Usuario():
+    def __init__(self, nome, email, senha, profissao, sexo, data_nasc):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.profissao = profissao
+        self.sexo = sexo
+        self.data_nasc = data_nasc
 
-cursor.executemany("""
-INSERT INTO TB_Usuario (id, nome, email, senha, profissao, sexo, data_nasc)
-VALUES (?, ?, ?, ?, ?, ?, ?)
-""", lista)
+    def inserir(self):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+          INSERT INTO TB_Usuario (nome, email, senha, profissao, sexo, data_nasc)
+          VALUES (self.nome, self.email, self.senha, self.profissao,
+           self.sexo, self.data_nasc)
+        ''')
+        conn.commit()
+        conn.close()
 
-cursor.execute("""
-SELECT * FROM TB_Usuario;
-""")
+    def listar(self):
+        usuarios = []
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT * FROM TB_Usuario;
+        """)
+        for linha in cursor.fetchall():
+            nome = linha[1]
+            email = linha[2]
+            senha = linha[3]
+            profissao = linha[4]
+            sexo = linha[5]
+            data_nasc = linha[6]
+            usuario = Usuario(nome, email, senha, profissao, sexo, data_nasc)
+            usuarios.append(usuario)
 
-for linha in cursor.fetchall():
-    print(linha)
+        conn.close()
 
-id_user = 1
-novo_email = 'bezerrinha@gmail.com'
-novo_senha = '12'
+        return usuarios
 
-cursor.execute("""
-UPDATE TB_Usuario
-SET email = ?, senha = ?
-WHERE id = ?
-""", (novo_email, novo_senha, id_user))
+    def deletar(self, id):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        id = input("")
+        cursor.execute("""
+        DELETE FROM TB_Usuario
+        WHERE id = ?
+        """, (id))
+        conn.commit()
+        conn.close()
 
-cursor.execute("""
-DELETE FROM TB_Usuario
-WHERE id = ?
-""", (id_user,))
 
-conn.commit()
-
-conn.close()
+    def atualizar(self, id, nome, email, senha, profissao, sexo, data_nasc):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        UPDATE TB_Usuario
+        SET email = ?, senha = ?, nome = ?, profissao = ?, sexo = ?, data_nasc = ?
+        WHERE id = ?
+        """, (email, senha, nome, profissao, sexo, data_nasc, id))
+        conn.commit()
+        conn.close()
