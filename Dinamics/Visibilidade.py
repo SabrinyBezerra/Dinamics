e90792eb20adf1759265a96dda536253f6025d69
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-11-07 09:55:48 -0300

    Update Visibilidade.py

--- Dinamics/Visibilidade.py
+++ Dinamics/Visibilidade.py
@@ -2,37 +2,28 @@ import sqlite3
 conn = sqlite3.connect('dinamics.db')
 cursor = conn.cursor()
 
-lista = [(1, True, False),
-         (2, False, True),
-         (3, False, False)]
+class Visibilidade():
+    def __init__ (self, publico, privado):
+        self.publico = publico
+        self.privado = privado
 
-cursor.executemany("""
-INSERT INTO TB_Visibilidade (id, publico, privado)
-VALUES (?, ?, ?)
-""", lista)
+    def inserir(self):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO TB_Mensagem (publico, privado)
+        VALUES (self.publico, self.privado)
+        """)
+        conn.commit()
+        conn.close()
 
-cursor.execute("""
-SELECT * FROM TB_Visibilidade;
-""")
-
-for linha in cursor.fetchall():
-    print(linha)
-
-id_vis = 1
-novo_publico = False
-novo_privado = True
-
-cursor.execute("""
-UPDATE TB_Visibilidade
-SET publico = ?, privado = ?
-WHERE id = ?
-""", (novo_publico, novo_privado, id_vis))
-
-cursor.execute("""
-DELETE FROM TB_Visibilidade
-WHERE id = ?
-""", (id_vis,))
-
-conn.commit()
-
-conn.close()
+    def atualizar(self, publico, privado):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        UPDATE TB_Usuario
+        SET publico = ?, privado = ?
+        WHERE id = ?
+        """, (publico, privado, id))
+        conn.commit()
+        conn.close()
