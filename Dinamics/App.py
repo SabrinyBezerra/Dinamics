e1bc4027755d2b1bb4d7c0f5df06c4649ce85848
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-11-14 23:40:16 -0200

    Update App.py

--- Dinamics/App.py
+++ Dinamics/App.py
@@ -1,10 +1,35 @@
 from Model.Usuario import Usuario
 
 def Menu():
-    print("///// D I N A M I C S /////")
-    print("     1 - FAZER CADASTRO    ")
+    print("\ \ \ \ D I N A M I C S / / / / \n"
+          " 1 - Criar conta \n"
+          " 2 - Sair")
+
+def criarUsuarioTB():
+    import sqlite3
+    conn = sqlite3.connect('dinamics.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE TB_Usuario (
+                   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                   nome VARCHAR(50) NOT NULL,
+                   email VARCHAR(100) NOT NULL,
+                   senha VARCHAR(50) NOT NULL,
+                   profissao TEXT,
+                   sexo TEXT,
+                   data_nasc DATE
+    );
+    """)
+
+    conn.commit()
+
+    conn.close()
 
 def FazerCadastro():
+
+    criarUsuarioTB()
+
     nome = input("Digite seu nome completo: ")
     email = input("Digite seu email: ")
     profissao = input("Digite sua profissão: ")
@@ -20,7 +45,22 @@ def main(args = []):
 
     Menu()
 
+    continuar = True
 
+    while continuar:
+        try:
+            op = int(input("digite a opção: "))
+
+            if (op == 1):
+                FazerCadastro()
+
+            elif (op == 2):
+                continuar = False
+            else:
+                print("Opção inválida!")
+
+        except ValueError:
+            print("Ops! Digite um valor válido")
 
 if (__name__ == "__main__"):
     main()
