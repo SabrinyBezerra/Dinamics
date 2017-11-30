faca02edb328bcda513c214c93f7375fe3e28321
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-11-30 09:13:48 -0200

    Update App.py

--- FuncionalidadeBD(31-11-2017)/App.py
+++ FuncionalidadeBD(31-11-2017)/App.py
@@ -1,6 +1,23 @@
 from Model.Usuario import Usuario
 import sqlite3
 
+def cadastrarUsuario():
+
+    #Solicitando os dados para cadastramento do usuario
+
+    usuario = []
+    nome = input("Digite seu nome:  ")
+    email = input("Digite seu email: ")
+    senha = input("Crie uma senha de usuário: ")
+    profissao = input("Digite sua profissão: ")
+    sexo = input("Digite seu sexo: F/M/PREFIRO NÃO DIZER")
+    data_nasc = input("Digite sua data de nascimento: ")
+    usuario = Usuario(nome, email, senha, profissao, sexo, data_nasc)
+
+    #Inserindo dados obtidos na tabela
+
+    Usuario.inserir(usuario)
+
 def menu():
     escolha = int(input('''
     MENU - D I N A M I C S -
@@ -8,7 +25,7 @@ def menu():
 
 escolha: '''))
     if escolha == 1:
-        print('Opção A escolhida')
+        cadastrarUsuario()
     else:
         print('Escolha invalida, tente novamente')
         menu()
