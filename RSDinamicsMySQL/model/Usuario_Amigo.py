7c75f5c8f446cd111fdcabe724b932f9440324ff
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-12-08 20:56:23 -0300

    Add files via upload

---
+++ RSDinamicsMySQL/model/Usuario_Amigo.py
@@ -0,0 +1,7 @@
+class Usuario_Amigo:
+    def __init__(self, nome_amigo, id_Amigo, id_Usuario, Amizade, Solicitacao):
+        self.nome_amigo = nome_amigo
+        self.id_Amigo = id_Amigo
+        self.id_Usuario = id_Usuario
+        self.Amizade = Amizade
+        self.Solicitacao = Solicitacao
