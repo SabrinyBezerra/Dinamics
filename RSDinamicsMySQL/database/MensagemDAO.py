6e2c9ae81a31a5b1780a9c6cb7ab8b33358bd388
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-12-08 21:53:29 -0200

    Update MensagemDAO.py

--- RSDinamicsMySQL/database/MensagemDAO.py
+++ RSDinamicsMySQL/database/MensagemDAO.py
@@ -1,9 +1,41 @@
 from model.Mensagem import Mensagem
+from database.configDB import config
+import mysql.connector
 
 class MensagemDAO():
 
     def inserirMensagem(self, mensagem: Mensagem):
-        pass
+        try:
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+            query = "INSERT INTO mensagem(texto, tipo, id_amigo, emoji)" \
+                    "VALUES (%s, %s, %i, %i)"
+            values = (mensagem.texto, mensagem.tipo, mensagem.id_amigo, mensagem.emoji)
+            cursor.execute(query, values)
+            conn.commit()
+        except mysql.connector.Error as error:
+            print(error)
+        finally:
+            cursor.close()
+            conn.close()
 
     def listarMensagem(self):
-        pass
+        try:
+            mensagens = []
+            conn = mysql.connector.connect(**config)
+            cursor = conn.cursor()
+            query = "SELECT * FROM mensagem"
+            mensagens.append(query)
+            for linha in mensagens():
+                texto = linha[1]
+                tipo = linha[2]
+                id_amigo = linha[3]
+                emoji = linha[4]
+                mensagens = Mensagem(texto, tipo, id_amigo, emoji)
+
+        except mysql.connector.Error as error:
+            print(error)
+
+        finally:
+            cursor.close()
+            conn.close()
