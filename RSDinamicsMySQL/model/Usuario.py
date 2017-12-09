2a33d897b77ba46fedbb8f6479fee05b7ea808b3
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-12-09 00:46:22 -0200

    Update Usuario.py

--- RSDinamicsMySQL/model/Usuario.py
+++ RSDinamicsMySQL/model/Usuario.py
@@ -1,12 +1,13 @@
 from model.Pessoa import Pessoa
 
 class Usuario(Pessoa):
-    def __init__(self, id_usuario, nome, nascimento, genero, email, senha):
+    def __init__(self, id_usuario, nome, nascimento, genero, email, senha, profissao):
         super(Usuario, self).__init__(nome, genero)
         self.id_usuario = id_usuario
         self.nascimento = nascimento
         self.email = email
         self.senha = senha
+        self.profissao = profissao
 
     def __str__(self):
         return "Usuario <%s>" % (self.nome)
